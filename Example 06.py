bbb72b5e06f7bb1f60b4ac4fdb7a3c9e8eb31853
Amir Abed <74647339+amirabed2024@users.noreply.github.com>
2022-08-27 18:10:04 +0430

    Delete Example 06.py

--- Example 06.py
+++
@@ -1,9 +0,0 @@
-Number=int(input("Get Number:"))
-Counter=0
-if(Number==0):
-    Counter+=1
-while(Number>0):
-    if(Number%10==0):
-        Counter+=1
-    Number=Number//10
-print("Tedad Sefr:",Counter)
