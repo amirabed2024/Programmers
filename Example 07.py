66ebfff6a6ab35bd4552c472de17c098143d435c
Amir Abed <74647339+amirabed2024@users.noreply.github.com>
2022-08-27 18:10:21 +0430

    Delete Example 07.py

--- Example 07.py
+++
@@ -1,7 +0,0 @@
-# Empower two numbers without using the power symbol
-Number_A,Number_B=[int(x)for x in input("Get Number A and B:").split(" ")]
-while(Number_B>1):
-    Number_A=Number_A*Number_A
-    Number_B-=1
-    
-print(Number_A)
